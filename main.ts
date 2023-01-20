d9ac9d986ac634e4880f3f06a21a57294f9ec6ea
Ilikedortios547 <122539726+Ilikedortios547@users.noreply.github.com>
2023-01-20 08:42:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showString("oh no")
+})
+input.onGesture(Gesture.SixG, function () {
+    basic.showString("ow")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . . . # .
+        . . . . .
+        . . . # .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+music.playMelody("C5 C E A E B C G ", 120)
